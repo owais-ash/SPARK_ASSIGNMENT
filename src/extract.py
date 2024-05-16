42a45186250ab1c478f21119385c93b35c7050fc
owais-ash <158836234+owais-ash@users.noreply.github.com>
2024-05-16 15:58:06 +0530

    Update extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,8 +1,16 @@
 import requests
 import csv
+import yaml
+
+
+def load_config():
+    with open('config.yaml', 'r') as file:
+        return yaml.safe_load(file)
+
+config = load_config()
 
 def fetch_country_data(country_name):
-    url = f'https://disease.sh/v3/covid-19/countries/{country_name}'
+    url = f"{config['api_url']}{country_name}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -16,15 +24,17 @@ countries = ["India", "USA",  "China", "Russia", "Brazil", "France", "Germany", 
              "Sweden", "Norway", "Ghana"]
 
 # Open CSV file for writing
-with open(data, 'w', newline='') as file:
-    writer = csv.writer(file)
+def pull():
+    with open(data, 'w', newline='') as file:
+        writer = csv.writer(file)
 
-    # Write header row
-    writer.writerow(['Country', 'Cases', 'Deaths', 'Recovered', 'Active_Cases', 'Critical_Cases'])
+        # Write header row
+        writer.writerow(['Country', 'Cases', 'Deaths', 'Recovered', 'Active_Cases', 'Critical_Cases'])
 
-    # Iterate over countries and fetch data
-    for country in countries:
-        country_data = fetch_country_data(country)
-        if country_data:
-            writer.writerow([country_data['country'], country_data['cases'], country_data['deaths'],
-                             country_data['recovered'], country_data['active'], country_data['critical']])
+        # Iterate over countries and fetch data
+        for country in countries:
+            country_data = fetch_country_data(country)
+            if country_data:
+                writer.writerow([country_data['country'], country_data['cases'], country_data['deaths'],
+                                country_data['recovered'], country_data['active'], country_data['critical']])
+pull()
